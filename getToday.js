0d2c5a4e3149812608030dac1ec136366cd518dd
kensuke suzuki <35k.0101@gmail.com>
2020-10-05 23:22:44 +0900

    2020/10/5

---
+++ getToday.js
@@ -0,0 +1,44 @@
+// 関数宣言
+function hoge() {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+
+  return year + "年" + month + "月" + day + "日";
+}
+
+// 関数式
+let hoge = function () {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+
+  return year + "年" + month + "月" + day + "日";
+}
+
+// 匿名関数＋コールバック関数
+function confirmed(fn) {
+  if (window.confirm("日付を取得しますか？")) {
+    fn();
+  }
+}
+confirmed(function () {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+  console.log(year + "年" + month + "月" + day + "日")
+})
+
+
+// アロー関数
+let hoge = () => {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+
+  return year + "年" + month + "月" + day + "日";
+}
